779d71297913d4a579f2f965591023e9ac9dbb9a
GeOnE <decodingrg@gmail.com>
2020-02-18 23:50:25 -0300

    Arreglado js para intervalos e interaccion.

--- js/slider.js
+++ js/slider.js
@@ -1,29 +1,64 @@
-var slideIndex = 0;
-var slides = document.getElementsByClassName("slide"); /*Llama a todos los elementos con la clase "slide".*/
-var pos=document.getElementsByClassName("pos")[0]; /*Llama al primer elemento con la clase "pos".*/
+let slideIndex = 0;
+let inter;
+const slides = document.getElementsByClassName("slide"); /*Llama a todos los elementos con la clase "slide".*/
+const pos=document.getElementsByClassName("pos")[0]; /*Llama al primer elemento con la clase "pos".*/
+const botizq=document.getElementsByClassName("bot izq")[0];
+const botder=document.getElementsByClassName("bot der")[0];
+
 for(var i=0; i<slides.length; i++) /*Repite el ciclo por cada elemento con la clase "slide".*/
   {
-    var dot=document.createElement("span");/*Crea el elemento <span>*/
+    const dot=document.createElement("span");/*Crea el elemento <span>*/
     dot.setAttribute("class", "dot"); /*Le agrega la clase "dot"*/
     pos.appendChild(dot); /*Lo introduce en el contenedor "pos".*/
   }
 
-mostrarSlides();
+const dots = document.getElementsByClassName("dot"); /*Llama a todos los elementos con la clase "dot"*/
 
-function mostrarSlides() {
-  var i;
-  var dots = document.getElementsByClassName("dot"); /*Llama a todos los elementos con la clase "dot"*/
-  for (i = 0; i < slides.length; i++) /*Repite el ciclo por cada elemento con la clase "slide".*/
-  	{
-    	slides[i].style.display = "none"; /*Oculta todos los slides*/  
-  	}
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1} /*Si el slideIndex es mayor a la cantidad de slides, vuelve a la posicion 1.*/   
-  for (i = 0; i < dots.length; i++)
-  	{
-    	dots[i].className = dots[i].className.replace("select", ""); /*Le reemplaza la clase "select" a todos los "dot" por una clase vacia.*/
-  	}
-  slides[slideIndex-1].style.display = "grid"; /*Cambia al "slide" para que sea visible*/  
-  dots[slideIndex-1].className += " select"; /*Le agrega la clase "select" a el "dot" donde se posiciona.*/
-  setTimeout(mostrarSlides,3000);
-}
+botizq.onclick=function(){cambio(0)};
+botder.onclick=function(){cambio(1)};
+
+mostrar();
+
+function cambio(num)
+  {
+    clearTimeout(inter);
+    if (num==0)
+      {
+        slideIndex=slideIndex-1;
+        if (slideIndex < 0)
+            {
+              slideIndex=slides.length-1;
+            }
+      }
+    else
+      {
+        slideIndex=slideIndex+1;
+        if (slideIndex > slides.length-1)
+          { 
+            slideIndex = 0;
+          }
+      }
+    mostrar();
+  }
+
+function mostrar()
+  {
+    ocultar();
+    slides[slideIndex].style.display = "grid"; /*Cambia al "slide" para que sea visible*/  
+    dots[slideIndex].className += " select"; /*Le agrega la clase "select" a el "dot" donde se posiciona.*/
+    intervalo();
+  }
+
+function ocultar()
+  {
+    for (let i = 0; i < dots.length; i++)
+      {
+        dots[i].className = dots[i].className.replace("select", ""); /*Le reemplaza la clase "select" a todos los "dot" por una clase vacia.*/
+        slides[i].style.display = "none"; /*Oculta todos los slides*/  
+      }
+  }
+
+function intervalo()
+  {
+    inter=setTimeout(function(){cambio(1)},10000);
+  }
